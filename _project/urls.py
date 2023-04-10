3868c593f14f34df50f1d4dd9673f29ae5f16f2f
Endang Ismaya <endang.ismaya@gmail.com>
2023-04-10 16:20:48 +0700

    add postgresql setting

--- _project/urls.py
+++ _project/urls.py
@@ -7,7 +7,10 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
+from . import views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
