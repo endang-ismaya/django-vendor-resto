4ffeb4cd4d7212d1637be746c2d9ca08a53438d7
Endang Ismaya <endang.ismaya@gmail.com>
2023-04-13 22:48:40 +0700

    move static to _project/static/

--- _project/settings.py
+++ _project/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "_project/static")]
 
 # MEDIA
 MEDIA_URL = "/media/"
